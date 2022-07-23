44b0c7a552f25d8a50def7cc055071269a963787
StepanovKirill <st.kirilius@mail.ru>
2022-07-23 21:15:19 +0500

    unit and integration tests added

---
+++ src/services/reducers/order-reducer.test.ts
@@ -0,0 +1,33 @@
+import {
+  POST_ORDER_REQUEST, 
+  POST_ORDER_SUCCESS, 
+  POST_ORDER_FAILED,
+  CLOSE_ORDER,
+  RESET_ORDER_NUMBER,
+  TOrderActions
+} from '../actions/order'
+import { orderInitialState, orderReducer } from './order-reducer';
+
+describe("Тест отправки заказа", () => {
+  it("Проверка начального состояния", () => {
+    expect(orderReducer(undefined, {} as TOrderActions))
+    .toEqual(orderInitialState);
+  })  
+  it('Отправка заказа', () => {
+    expect(orderReducer(orderInitialState, {type: POST_ORDER_REQUEST}))
+    .toEqual({...orderInitialState, orderRequest: true});
+
+    expect(orderReducer({...orderInitialState, orderRequest: true}, {type: POST_ORDER_FAILED}))
+    .toEqual({...orderInitialState, orderFailed: true});
+
+    expect(orderReducer({...orderInitialState, orderRequest: true}, {type: POST_ORDER_SUCCESS, orderNumber: 422}))
+    .toEqual({...orderInitialState, orderNumber: 422});
+
+    expect(orderReducer({...orderInitialState, orderNumber: 4242}, {type: RESET_ORDER_NUMBER}))
+    .toEqual(orderInitialState);
+  });
+  it("закрытие модалки", () => {
+    expect(orderReducer({...orderInitialState, orderNumber: 4242}, {type: CLOSE_ORDER}))
+    .toEqual(orderInitialState)
+  });
+})
